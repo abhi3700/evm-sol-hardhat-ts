0adb9136a16100a32c69aa0ce55e5a2af393f2a0
Abhijit Roy <dalvath3700@gmail.com>
2021-08-21 20:05:56 +0530

    openzeppelin added as devDependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -81,7 +81,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.6.12",
+        version: "0.8.0",
         settings: {
           optimizer: {
             enabled: true,
